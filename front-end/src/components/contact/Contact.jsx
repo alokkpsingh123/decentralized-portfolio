7fa42a447a87c9be0d5661f31a3c9eaaa94a6177
Alok Singh <54284060+alokkpsingh123@users.noreply.github.com>
2023-05-23 21:41:16 +0530

    Final Commit

--- front-end/src/components/contact/Contact.jsx
+++ front-end/src/components/contact/Contact.jsx
@@ -1,21 +1,18 @@
-import './Contact.css'
+import "./Contact.css";
 
-const Contact = ({state}) => {
- 
-    return (
-        <section className="contact-section">
-            <h1 className="title">
-                Interested?
-                Let's Get In Touch!
-            </h1>
-            <a href="#" target='_blank' rel="noopener noreferrer">
-                <button className="downlodeBTN">
-                    View Resume
-                </button>
-            </a>
+const Contact = ({ state }) => {
+  return (
+    <section className="contact-section">
+      <h1 className="title">Interested? Let's Get In Touch!</h1>
+      <a
+        href="https://ipfs.io/ipfs/QmaSywY1Chx3joG3WKqkS7ebN5bxCSkpsMXMJwbwVBEoHV"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="downlodeBTN">View Resume</button>
+      </a>
+    </section>
+  );
+};
 
-        </section>
-    )
-}
-
-export default Contact
+export default Contact;
